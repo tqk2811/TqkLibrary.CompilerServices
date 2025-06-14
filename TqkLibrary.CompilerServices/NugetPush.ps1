2fceab34cae89ceedb7104b363a45d89c39a2825
tqk2811 <tqk2811@gmail.com>
2025-06-14 15:30:16 +0700

    update build script

--- TqkLibrary.CompilerServices/NugetPush.ps1
+++ TqkLibrary.CompilerServices/NugetPush.ps1
@@ -1,3 +1,11 @@
 ﻿$key=$env:nugetKey
-nuget pack .\TqkLibrary.CompilerServices.nuspec -Symbols -OutputDirectory .\TqkLibrary.CompilerServices\bin\Release
-iex "nuget push .\TqkLibrary.CompilerServices\bin\Release\ -ApiKey $($key) -Source https://api.nuget.org/v3/index.json"
+
+Remove-Item -Recurse -Force .\bin\Release\** -ErrorAction SilentlyContinue;
+nuget pack .\TqkLibrary.CompilerServices.nuspec -OutputDirectory .\bin\Release
+
+Write-Host "enter to push nuget"
+pause
+Write-Host "enter to confirm"
+pause
+
+nuget push .\bin\Release\ -ApiKey $($key) -Source https://api.nuget.org/v3/index.json
