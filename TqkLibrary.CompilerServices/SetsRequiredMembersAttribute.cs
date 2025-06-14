804f3cd36de1df931791308d18ebbc917b89a515
tqk2811 <tqk2811@gmail.com>
2025-06-14 15:37:47 +0700

    add SetsRequiredMembersAttribute

---
+++ TqkLibrary.CompilerServices/SetsRequiredMembersAttribute.cs
@@ -0,0 +1,11 @@
+﻿#if !NET7_0_OR_GREATER
+#pragma warning disable
+namespace System.Diagnostics.CodeAnalysis
+{
+    [System.AttributeUsage(System.AttributeTargets.Constructor, AllowMultiple = false, Inherited = false)]
+    internal sealed class SetsRequiredMembersAttribute : System.Attribute
+    {
+    }
+}
+#pragma warning enable
+#endif
