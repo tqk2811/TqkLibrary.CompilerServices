3b8b7b5e92150880d6ecbfab103720161af5d1a5
tqk2811 <tqk2811@gmail.com>
2025-06-14 15:23:36 +0700

    nuspec add readme

--- TqkLibrary.CompilerServices/NugetPush.ps1
+++ TqkLibrary.CompilerServices/NugetPush.ps1
@@ -1,1 +1,3 @@
-﻿.\..\CsharpNugetPush\CsharpNugetPush.ps1
+﻿$key=$env:nugetKey
+nuget pack .\TqkLibrary.CompilerServices.nuspec -Symbols -OutputDirectory .\TqkLibrary.CompilerServices\bin\Release
+iex "nuget push .\TqkLibrary.CompilerServices\bin\Release\ -ApiKey $($key) -Source https://api.nuget.org/v3/index.json"
